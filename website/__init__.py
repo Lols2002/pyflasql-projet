eedf29f892dcc21eba02042c74d73f2496bc9a76
Lols2002 <h.idjaha2@gmail.com>
2023-11-02 16:43:24 +0100

    project

--- website/__init__.py
+++ website/__init__.py
@@ -1,8 +1,16 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from os import path
+from flask_login import LoginManager
 
 db=SQLAlchemy()
 DB_NAME= "database.db"
+
+def create_database(app):
+    if not path.exists('website/' + DB_NAME):
+        with app.app_context():
+            db.create_all()
+        print('Created database!')
 
 def create_app():
     
@@ -16,5 +24,20 @@ def create_app():
 
     app.register_blueprint(views,url_prefix='/')
     app.register_blueprint(auth,url_prefix='/')
+
+    from .models import User, Note
+
+    create_database(app)
+    
+    login_manager = LoginManager()
+    login_manager.login_view = 'auth.login'
+    login_manager.init_app(app)
+
+    @login_manager.user_loader
+    def load_user(id):
+        return User.query.get(int(id))
+
+
     
     return app
+
